90f83b22e4bd43e803ee87ad6dcc99a61e886ff0
guisilvas <guilherme7soaress@gmail.com>
2023-05-16 09:54:07 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -6,5 +6,13 @@
     $assunto = $_POST['assunto'];
     $mensagem = $_POST['mensagem'];
 
-    $sql = 'INSERT INTO contato(nome, email, assunto, memsagem)'
+    $sql = "INSERT INTO contato(nome, email, assunto, mensagem) VALUES ('$nome', '$email', '$assunto', '$mensagem')";
+
+    if (mysqli_query($conexao, $sql)) {
+        echo "Agradecemos pelo seu contato!";
+    }
+    else {
+        echo "Erro" . mysqli_connect_error($conexao);
+    }
+    mysqli_close($conexao);
 ?>
